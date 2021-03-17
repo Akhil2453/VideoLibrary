e3bd65b7b8f353f52a4099fb56f1e230ed411a62
Akhil Menon <akhil.menon94@gmail.com>
2021-03-17 20:15:54 +0530

    updated ir code

--- ir.py
+++ ir.py
@@ -10,11 +10,9 @@ GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(aux_vcc, GPIO.OUT)
 GPIO.output(aux_vcc, GPIO.HIGH)
 
-def loop():
+while True:
 	a = GPIO.input(signal)
 	if (a == False):
 		print("Sensor is Working")
 	else:
 		print("Sensor is not working")
-
-loop()
