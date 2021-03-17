f233caf95cb21a57e7fd59f6a077552f9baeb31c
Akhil Menon <akhil.menon94@gmail.com>
2021-03-17 20:52:44 +0530

    converted BCA to BOARD

--- ir.py
+++ ir.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-aux_vcc = 23
-signal = 24
+aux_vcc = 16
+signal = 18
 a = 1
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(signal, GPIO.IN) #, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(aux_vcc, GPIO.OUT)
 GPIO.output(aux_vcc, GPIO.HIGH)
