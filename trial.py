6da7f18a983987637c76bc58fa91b961eec53896
Akhil Menon <akhil.menon94@gmail.com>
2021-03-27 11:02:01 +0530

    update12

--- trial.py
+++ trial.py
@@ -35,8 +35,8 @@ for i in files:
                 dim = (window_width, window_height)
                 cv2.resizeWindow('Frame',window_width, window_height)
                 cv2.imshow('Frame', cv2.resize(frame, dim, interpolation=cv2.INTER_AREA))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
 # When everything done, release the capture
 cap.release()
